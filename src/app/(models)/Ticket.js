64db7868f9e3566bff4e1cff06f26cdd30bf6119
THIVAGAR <thivagar.m@sysfore.com>
2024-04-05 17:28:27 +0530

    initial commit

---
+++ src/app/(models)/Ticket.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URL);
+console.log(process.env.MONGODB_URL);
+mongoose.Promise = global.Promise;
+
+const ticketSchema = new Schema(
+  {
+    tittle: String,
+    description: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", ticketSchema);
+
+export default Ticket;
